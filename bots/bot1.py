807bc6dfd413a04a56aba4f906eaf746cfa6de25
James <2509941b@student.gla.ac.uk>
2022-09-08 18:24:02 +0100

    improved function and method signatures

--- bots/bot1.py
+++ bots/bot1.py
@@ -1,7 +1,8 @@
-from ftx.rest.client import FtxClient
-from ftx.tradingfunctions import *
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+from ftx.tradingfunctions import *
 
 load_dotenv()
 
@@ -15,7 +16,8 @@ class Bot:
     
     def __init__(self):
         
-        self.botInfo = [input("Most recent type of confirmation signal? UT or DT?"), input("Trend catcher state? CD for red or CU for green.")]
+        self.botInfo = [input("Most recent type of confirmation signal? UT or DT?"),
+                        input("Trend catcher state? CD for red or CU for green.")]
         self.tradingViewInfo = None
         self.accRisk = 0.02
         self.port = 0
@@ -23,7 +25,7 @@ class Bot:
         for i in client.get_balances():
             self.port += i['total']
 
-    def update(self, data):
+    def update(self, data: str):
 
         self.tradingViewInfo = data.split()
         self.tradingViewInfo[0] = self.tradingViewInfo[0][:-4]
@@ -35,10 +37,12 @@ class Bot:
             else:
                 self.botInfo[0] = self.tradingViewInfo[1]
 
-        if (self.tradingViewInfo[1] == "UT" or self.tradingViewInfo[1] == "ES") and client.get_positions()[0]['size'] < 0:
+        if (self.tradingViewInfo[1] == "UT" or self.tradingViewInfo[1] == "ES") and\
+                client.get_positions()[0]['size'] < 0:
             exit_trade(self.tradingViewInfo[0], 'sell', client)
             
-        elif (self.tradingViewInfo[1] == "DT" or self.tradingViewInfo[1] == "EB") and client.get_positions()[0]['size'] > 0:
+        elif (self.tradingViewInfo[1] == "DT" or self.tradingViewInfo[1] == "EB") and\
+                client.get_positions()[0]['size'] > 0:
             exit_trade(self.tradingViewInfo[0], 'buy', client)
 
         if self.tradingViewInfo[1] == "OU":
@@ -53,7 +57,8 @@ class Bot:
         for i in client.get_balances():
             self.port += i['total']
 
-        if self.botInfo[0] == "UT" and self.botInfo[1] == "CU" and self.tradingViewInfo[1] == "OU" and client.get_positions()[0]['size'] == 0:
+        if self.botInfo[0] == "UT" and self.botInfo[1] == "CU" and self.tradingViewInfo[1] == "OU" and\
+                client.get_positions()[0]['size'] == 0:
             sl = calc_sl(self.tradingViewInfo[2], 'buy', 1)
             tp = calc_tp(self.tradingViewInfo[2], 'buy', 1)
             pos_size = calc_pos_size(port=self.port, entry_price=self.tradingViewInfo[2], side='buy',
@@ -61,19 +66,11 @@ class Bot:
             enter_buy(market=self.tradingViewInfo[0], price=self.tradingViewInfo[2], size=pos_size,
                       sl=sl, tp=tp, client=client)
 
-        elif self.botInfo[0] == "DT" and self.botInfo[1] == "CD" and self.tradingViewInfo[1] == "OD" and client.get_positions()[0]['size'] == 0:
+        elif self.botInfo[0] == "DT" and self.botInfo[1] == "CD" and self.tradingViewInfo[1] == "OD" and\
+                client.get_positions()[0]['size'] == 0:
             sl = calc_sl(self.tradingViewInfo[2], 'sell', 1)
             tp = calc_tp(self.tradingViewInfo[2], 'sell', 1)
             pos_size = calc_pos_size(port=self.port, entry_price=self.tradingViewInfo[2], side='sell',
                                      acc_risk=self.accRisk, sl=sl)
             enter_sell(market=self.tradingViewInfo[0], price=self.tradingViewInfo[2], size=pos_size,
                        sl=sl, tp=tp, client=client)
-
-    def handle_exit_signal(self, exit_type):
-        if client.get_positions()[0]['size'] != 0:
-            if exit_type == "ES":
-                exit_trade(self.tradingViewInfo[0], 'sell', client)
-                
-            elif exit_type == "EB":
-                exit_trade(self.tradingViewInfo[0], 'buy', client)
-
--- ftx/tradingfunctions.py
+++ ftx/tradingfunctions.py
@@ -1,30 +1,27 @@
-def calc_sl(price, side, sl_percent):
-    if side == 'sell':
-        sl = price + price * (sl_percent / 100)
-    elif side == 'buy':
-        sl = price - price * (sl_percent / 100)
-    return sl
+from ftx.rest.client import FtxClient
 
 
-def calc_tp(price, side, tp_percent):
-    if side == 'sell':
-        tp = price - price * (tp_percent / 100)
-    elif side == 'buy':
-        tp = price + price * (tp_percent / 100)
-    return tp
+def calc_sl(price: float, side: str, sl_percent: float) -> float:
+    assert side in ('buy', 'sell'), 'side must be buy or sell'
+    sl_percent_decimal = sl_percent / 100
+    return price + price * sl_percent_decimal if side == 'sell' else price - price * sl_percent_decimal
 
 
-def calc_pos_size(port, entry_price, side, acc_risk, sl):
+def calc_tp(price: float, side: str, tp_percent: float) -> float:
+    assert side in ('buy', 'sell'), 'side must be buy or sell'
+    tp_percent_decimal = tp_percent / 100
+    return price - price * tp_percent_decimal if side == 'sell' else price + price * tp_percent_decimal
+
+
+def calc_pos_size(port: float, entry_price: float, side: str, acc_risk: float, sl: float) -> float:
+    assert side in ('buy', 'sell'), 'side must be buy or sell'
     risk_amount = port * acc_risk
-    if side == 'sell':
-        sl_distance = sl - entry_price
-    else:
-        sl_distance = entry_price - sl
-    pos_size = risk_amount / sl_distance
-    return pos_size
+    return risk_amount / (sl - entry_price) if side == 'sell' else risk_amount / (entry_price - sl)
 
 
-def exit_trade(market, side, client):
+def exit_trade(market: str, side: str, client: FtxClient):
+    assert side in ('buy', 'sell'), 'side must be buy or sell'
+    assert any(i['name'] == f'{market}-PERP' for i in client.get_markets())
     if side == 'sell':
         client.place_order(
             market=f'{market}-PERP',
@@ -33,7 +30,7 @@ def exit_trade(market, side, client):
             reduce_only=True,
             size=client.get_positions()[0]['size'] + client.get_positions()[0]['size'] * 2
         )
-    elif side == 'buy':
+    else:
         client.place_order(
             market=f'{market}-PERP',
             side="sell",
@@ -43,7 +40,8 @@ def exit_trade(market, side, client):
         )
 
 
-def enter_buy(market, price, size, sl, tp, client):
+def enter_buy(market: str, price: float, size: float, client: FtxClient, sl: float = None, tp: float = None):
+    assert any(i['name'] == f'{market}-PERP' for i in client.get_markets())
     client.place_order(
         market=f'{market}-PERP',
         side="buy",
@@ -51,26 +49,29 @@ def enter_buy(market, price, size, sl, tp, client):
         size=size
     )
 
-    client.place_conditional_order(
-        market=f'{market}-PERP',
-        side="sell",
-        size=size + (size * 2),
-        type='stop',
-        reduce_only=True,
-        trigger_price=sl
-    )
+    if sl:
+        client.place_conditional_order(
+            market=f'{market}-PERP',
+            side="sell",
+            size=size + (size * 2),
+            type='stop',
+            reduce_only=True,
+            trigger_price=sl
+        )
 
-    client.place_conditional_order(
-        market=f'{market}-PERP',
-        side="sell",
-        size=size + (size * 2),
-        type='take_profit',
-        reduce_only=True,
-        trigger_price=tp
-    )
+    if tp:
+        client.place_conditional_order(
+            market=f'{market}-PERP',
+            side="sell",
+            size=size + (size * 2),
+            type='take_profit',
+            reduce_only=True,
+            trigger_price=tp
+        )
 
 
-def enter_sell(market, price, size, sl, tp, client):
+def enter_sell(market: str, price: float, size: float, client: FtxClient, sl: float = None, tp: float = None) -> None:
+    assert any(i['name'] == f'{market}-PERP' for i in client.get_markets())
     client.place_order(
         market=f'{market}-PERP',
         side="sell",
@@ -79,24 +80,26 @@ def enter_sell(market, price, size, sl, tp, client):
         size=size
     )
 
-    client.place_conditional_order(
-        market=f'{market}-PERP',
-        side="buy",
-        size=size + (size * 2),
-        type='stop',
-        reduce_only=True,
-        trigger_price=sl
-    )
+    if sl:
+        client.place_conditional_order(
+            market=f'{market}-PERP',
+            side="buy",
+            size=size + (size * 2),
+            type='stop',
+            reduce_only=True,
+            trigger_price=sl
+        )
 
-    client.place_conditional_order(
-        market=f'{market}-PERP',
-        side="buy",
-        size=size + (size * 2),
-        type='take_profit',
-        reduce_only=True,
-        trigger_price=tp
-    )
+    if tp:
+        client.place_conditional_order(
+            market=f'{market}-PERP',
+            side="buy",
+            size=size + (size * 2),
+            type='take_profit',
+            reduce_only=True,
+            trigger_price=tp
+        )
 
 
-def set_acc_risk(acc_risk, bot):
+def set_acc_risk(acc_risk: float, bot) -> None:
     bot.accRisk = acc_risk
