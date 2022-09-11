7e5b2332e4baad9ff94b4b485e98ab187f764a43
James <2509941b@student.gla.ac.uk>
2022-09-11 18:13:01 +0100

    fixed bugs

--- webhook.py
+++ webhook.py
@@ -1,12 +1,12 @@
+from threading import Thread
 from flask import Flask, request, abort
 from bots import bot1
 
+t1 = Thread(target=bot1.start_bot)
+t1.setDaemon(True)
+t1.start()
+
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    bot1.start_bot()
 
 
 @app.route('/webhook', methods=['POST'])
